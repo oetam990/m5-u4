0b449588c311d0d98fd72afbafc147618802cee8
Mateo Baracco <oetam991@gmail.com>
2023-07-19 22:52:09 -0300

    First commit

---
+++ routes/admin/login.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModels');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.render('admin/login', {layout: 'admin/layout'});
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+
+        var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+
+        if (data != undefined) {
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = router;
